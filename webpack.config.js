4f228ff0fce9ab6fe1219e090449096375d231f0
YukJiSoo <wltn3231@naver.com>
2020-01-09 16:36:35 +0900

    feat: output 속성 설정
    output 속성을 설정해주었습니다.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require("path");
 module.exports = {
 	context: path.resolve(__dirname, "src"),
 	entry: {
-		main: "./index.js"
+		app: "./index.js"
+	},
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "[name].bundle.js"
 	}
 };
